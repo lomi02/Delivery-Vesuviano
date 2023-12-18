09609ff1a073c79d4b6cc22ae2dce2b9ba49b042
Lorenzo Mirabella <lorenzo.mirabella001@studenti.uniparthenope.it>
2023-12-18 16:26:17 +0100

    Aggiustamento Frontend e semi-finalizzazione sistema di account e impostazioni

--- express/server.js
+++ express/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const crypto = require('crypto');
+const bcrypt = require('bcrypt');
 
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -23,6 +25,29 @@ const db = new sqlite3.Database(dbPath, (err) => {
     console.error('Errore nella connessione al database', err.message);
   } else {
     console.log('Connessione al database riuscita');
+  }
+});
+
+// API di fetching delle informazioni del cliente
+app.get('/api/user/:tokenid', async (req, res) => {
+  const tokenID = req.params.tokenid;
+  try {
+    const query = `
+      SELECT *
+      FROM CLIENTE
+             JOIN TOKEN ON CLIENTE.ID_CLIENTE = TOKEN.USER_ID
+      WHERE TOKEN.TOKEN_ID = :tokenid;
+    `;
+
+    const result = await sequelize.query(query, {
+      replacements: {tokenid: tokenID},
+      type: QueryTypes.SELECT,
+    });
+
+    res.json(result);
+  } catch (error) {
+    console.error('Errore durante il recupero dei dati del cliente:', error);
+    res.status(500).send('Errore interno del server');
   }
 });
 
@@ -67,14 +92,22 @@ app.get('/api/menu/:id_ristorante', async (req, res) => {
 app.post('/api/login', (req, res) => {
   const {email, password} = req.body;
 
-  // Replace this with your actual database query logic
-  db.get('SELECT * FROM CLIENTE WHERE EMAIL_CLIENTE = ? AND PASSWORD_CLIENTE = ?', [email, password], (err, row) => {
+  db.get('SELECT * FROM CLIENTE WHERE EMAIL_CLIENTE = ?', [email], (err, row) => {
     if (err) {
       console.error(err);
       res.status(500).json({success: false, message: 'Internal Server Error'});
     } else {
-      if (row) {
-        res.json({success: true, message: 'Login successful'});
+      if (row && bcrypt.compareSync(password, row.PASSWORD_CLIENTE)) {
+        const token = crypto.randomBytes(64).toString('hex');
+        const expirationTimestamp = Math.floor(Date.now() / 1000) + 60 * 30;
+        db.run('INSERT INTO TOKEN (USER_ID, TOKEN_STRING, EXPIRATION_TIMESTAMP) VALUES (?, ?, ?)', [row.ID_CLIENTE, token, expirationTimestamp], (err) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({success: false, message: 'Internal Server Error'});
+          } else {
+            res.json({success: true, message: 'Login successful', token: token});
+          }
+        });
       } else {
         res.json({success: false, message: 'Invalid credentials'});
       }
@@ -82,38 +115,150 @@ app.post('/api/login', (req, res) => {
   });
 });
 
+// Sistema di verifica di token
+app.post('/api/verify-token', (req, res) => {
+  const token = req.headers.authorization;
+  console.log('Token from request:', token);
+
+  db.get('SELECT * FROM TOKEN WHERE TOKEN_STRING = ?', [token], (err, row) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({success: false, message: 'Internal Server Error'});
+    } else {
+      if (row && Math.floor(Date.now() / 1000) < row.EXPIRATION_TIMESTAMP) {
+        db.get('SELECT * FROM CLIENTE WHERE ID_CLIENTE = ?', [row.USER_ID], (err, user) => {
+          if (err) {
+            console.error(err);
+            res.status(500).json({success: false, message: 'Internal Server Error'});
+          } else {
+            res.json({success: true, user: user});
+          }
+        });
+      } else {
+        // Invia una risposta anche quando il token non è valido o è scaduto
+        res.json({success: false, message: 'Invalid or expired token'});
+      }
+    }
+  });
+});
+
+// Sistema middleware di Token
+const ignorePaths = ['/api/register'];
+app.use((req, res, next) => {
+  // Se il percorso della richiesta corrente è nell'array dei percorsi da ignorare, passa al prossimo middleware
+  if (ignorePaths.includes(req.path)) {
+    next();
+  } else {
+    const token = req.headers['authorization'];
+
+    if (!token) {
+      res.status(401).json({success: false, message: 'No token provided'});
+    } else {
+      db.get('SELECT * FROM TOKEN WHERE TOKEN_STRING = ? AND EXPIRATION_TIMESTAMP > ?', [token, Math.floor(Date.now() / 1000)], (err, row) => {
+        if (err) {
+          console.error(err);
+          res.status(500).json({success: false, message: 'Internal Server Error'});
+        } else {
+          if (row) {
+            req.userId = row.USER_ID;
+            next();
+          } else {
+            res.status(401).json({success: false, message: 'Invalid token'});
+          }
+        }
+      });
+
+      // Pulizia dei token scaduti
+      db.run('DELETE FROM TOKEN WHERE EXPIRATION_TIMESTAMP <= ?', [Math.floor(Date.now() / 1000)], (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('Expired tokens cleaned up');
+        }
+      });
+    }
+  }
+});
+
 // API di Registrazione
 app.post('/api/register', (req, res) => {
-  const { nome, cognome, email, password, via, citta, cap, citofono } = req.body;
+  const {nome, cognome, email, password, via, citta, cap, citofono} = req.body;
+  const saltRounds = 10;
 
-  // Esecuzione dell'istruzione SQL di INSERT per aggiungere un nuovo utente
-  const insertClienteQuery = `
-    INSERT INTO CLIENTE (NOME_CLIENTE, COGNOME_CLIENTE, EMAIL_CLIENTE, PASSWORD_CLIENTE, VIA_CLIENTE, CITTA_CLIENTE, CAP_CLIENTE, CITOFONO_CLIENTE)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-  `;
+  bcrypt.hash(password, saltRounds, (err, hashedPassword) => {
+    if (err) {
+      console.error('Errore durante l\'hashing della password:', err);
+      res.status(500).json({message: 'Errore durante l\'hashing della password'});
+    } else {
+      const insertClienteQuery = `
+        INSERT INTO CLIENTE (NOME_CLIENTE, COGNOME_CLIENTE, EMAIL_CLIENTE, PASSWORD_CLIENTE, VIA_CLIENTE, CITTA_CLIENTE,
+                             CAP_CLIENTE, CITOFONO_CLIENTE)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+      `;
 
-  const beginTransaction = 'BEGIN TRANSACTION';
-  const commitTransaction = 'COMMIT';
+      db.run(insertClienteQuery, [nome, cognome, email, hashedPassword, via, citta, cap, citofono], function (err) {
+        if (err) {
+          console.error('Errore durante la registrazione:', err);
+          res.status(500).json({message: 'Errore durante la registrazione'});
+        } else {
+          console.log('Registrazione avvenuta con successo');
+          res.status(201).json({message: 'Registrazione avvenuta con successo'});
+        }
+      });
+    }
+  });
+});
 
-  try {
-    // Inizia la transazione
-    db.run(beginTransaction);
+// API per l'aggiornamento del campo password
+app.post('/api/update/password', (req, res) => {
+  let sql = `UPDATE CLIENTE SET PASSWORD_CLIENTE = ? WHERE ID_CLIENTE = ?`;
+  db.run(sql, [req.body.password, req.body.userId], function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.json({ success: true, message: 'Password Aggiornata con successo.' });
+  });
+});
 
-    // Inserisci dati in CLIENTE
-    db.run(insertClienteQuery, [nome, cognome, email, password, via, citta, cap, citofono]);
+// API per l'aggiornamento del campo email
+app.post('/api/update/password', (req, res) => {
+  let sql = `UPDATE CLIENTE SET EMAIL_CLIENTE = ? WHERE ID_CLIENTE = ?`;
+  db.run(sql, [req.body.password, req.body.userId], function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.json({ success: true, message: 'Email Aggiornata con successo.' });
+  });
+});
 
-    // Concludi la transazione
-    db.run(commitTransaction);
+app.post('/api/update/address', (req, res) => {
+  let sql = `UPDATE CLIENTE SET VIA_CLIENTE = ?, CITTA_CLIENTE = ?, CAP_CLIENTE = ?, CITOFONO_CLIENTE = ? WHERE ID_CLIENTE = ?`;
+  db.run(sql, [req.body.via, req.body.citta, req.body.cap, req.body.citofono, req.body.userId], function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.json({ success: true, message: 'Indirizzo Aggiornato con successo.' });
+  });
+});
 
-    console.log('Registrazione avvenuta con successo');
-    res.status(201).json({ message: 'Registrazione avvenuta con successo' });
-  }
-  catch (error) {
-    // In caso di errore, esegui il rollback della transazione
-    db.run('ROLLBACK');
-    console.error('Errore durante la registrazione:', error);
-    res.status(500).json({ message: 'Errore durante la registrazione' });
-  }
+app.post('/api/logout', (req, res) => {
+  let sql = `DELETE FROM TOKEN WHERE USER_ID = ?`;
+  db.run(sql, [req.body.userId], function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.json({ success: true, message: 'Effettuato il Logout.' });
+  });
+});
+
+app.delete('/api/delete', (req, res) => {
+  let sql = `DELETE FROM CLIENTE WHERE ID_CLIENTE = ?`;
+  db.run(sql, [req.body.userId], function(err) {
+    if (err) {
+      return console.error(err.message);
+    }
+    res.json({ success: true, message: 'Account Eliminato con Successo.' });
+  });
 });
 
 // Avvio del server
--- js/main.js
+++ js/main.js
@@ -1,135 +1,3 @@
-document.addEventListener('DOMContentLoaded', async function () {
-  try {
-
-    // Chiamata al backend per ottenere i dati dei locali
-    const response = await fetch('http://localhost:3000/api/ristorante');
-    const ristoranti = await response.json();
-
-    console.log('Dati ottenuti:', ristoranti);
-
-    // Aggiorna l'UI con i dati ottenuti
-    aggiornaGrigliaRistoranti(ristoranti);
-
-  } catch (error) {
-    console.error('Errore nel recupero dei dati del ristorante:', error);
-  }
-});
-
-async function fetchRistoranti() {
-  try {
-    const response = await fetch('http://localhost:3000/api/ristorante');
-    const ristoranti = await response.json();
-    aggiornaGrigliaRistoranti(ristoranti);
-
-    // Nascondi altre schede e mostra la scheda dei ristoranti
-    document.getElementById('griglia-ristoranti').style.display = 'block';
-
-  } catch (error) {
-    console.error('Errore durante il recupero e la visualizzazione dei ristoranti:', error);
-  }
-}
-
-// Funzione per aggiornare la griglia dei locali
-function aggiornaGrigliaRistoranti(ristoranti) {
-  const grigliaRistoranti = document.getElementById('contenitore-locale');
-  grigliaRistoranti.innerHTML = '';
-
-  ristoranti.forEach(locale => {
-    const localeCard = creaCartaLocale(locale);
-    localeCard.addEventListener('click', () => mostraDettagliLocale(locale));
-    grigliaRistoranti.appendChild(localeCard);
-  });
-}
-
-// Funzione per creare schede di locali
-function creaCartaLocale(locale) {
-  const card = document.createElement('div');
-  card.classList.add('locale-card');
-
-  // Aggiungi immagine del locale alla card
-  const imageElement = document.createElement('img');
-  imageElement.src = locale.IMG_URL_RISTORANTE;
-
-  // Aggiungi dettagli del locale alla card
-  const nameElement = document.createElement('h2');
-  nameElement.textContent = locale.NOME_RISTORANTE;
-
-  // Aggiungi elementi alla card
-  card.appendChild(imageElement);
-  card.appendChild(nameElement);
-
-  return card;
-}
-
-// Funzione per mostrare i dettagli del locale
-async function mostraDettagliLocale(locale) {
-  const dettagliLocale = document.getElementById('dettagli-locale');
-  dettagliLocale.innerHTML = '';
-
-  // Aggiungi dettagli del locale al lato destro
-  const nameElement = document.createElement('h2');
-  nameElement.textContent = locale.NOME_RISTORANTE;
-  nameElement.classList.add('nome-locale');
-  dettagliLocale.appendChild(nameElement);
-
-  // Nascondi altre schede e mostra la scheda dei locali
-  document.getElementById('griglia-ristoranti').style.display = 'none';
-
-  // Mostra la sezione dei dettagli del locale con animazione
-  dettagliLocale.style.display = 'block';
-  dettagliLocale.style.transform = 'translateY(0)';
-
-  // Fai una chiamata al tuo backend per ottenere il menù del ristorante
-  const response = await fetch('http://localhost:3000/api/menu/' + locale.P_IVA_RISTORANTE);
-  const menu = await response.json();
-
-  // Aggiungi il menù al dettaglio del locale
-  const menuElement = document.createElement('div');
-  menuElement.classList.add('contenitore-menu');
-  menu.forEach(item => {
-    const itemElement = creaCartaPiatto(item);
-    menuElement.appendChild(itemElement);
-  });
-  dettagliLocale.appendChild(menuElement);
-}
-
-// Funzione per creare schede di piatti
-function creaCartaPiatto(piatto) {
-  const card = document.createElement('div');
-  card.classList.add('piatto-card');
-
-  // Aggiungi immagine del piatto alla card
-  const imageElement = document.createElement('img');
-  imageElement.src = piatto.IMG_URL_PIATTO;
-
-  // Aggiungi dettagli del piatto alla card
-  const nameElement = document.createElement('h2');
-  nameElement.textContent = piatto.NOME_PIATTO;
-
-  const priceElement = document.createElement('p');
-  priceElement.textContent = piatto.PREZZO.toFixed(2) + '€'; // Limita il prezzo a due cifre decimali
-
-  const descriptionElement = document.createElement('p');
-  descriptionElement.textContent = piatto.DESCRIZIONE;
-
-  // Aggiungi pulsante Acquista
-  const buyButton = document.createElement('button');
-  buyButton.textContent = 'Acquista';
-  buyButton.addEventListener('click', () => {
-    // Aggiungi il piatto al carrello
-    aggiungiAlCarrello(piatto);
-  });
-
-  // Aggiungi elementi alla card
-  card.appendChild(imageElement);
-  card.appendChild(nameElement);
-  card.appendChild(priceElement);
-  card.appendChild(descriptionElement);
-  card.appendChild(buyButton);
-
-  return card;
-}
-
 // Funzione per gestire il login
 async function login() {
   try {
@@ -146,12 +14,58 @@ async function login() {
 
     const result = await response.json();
     if (result.success) {
-      alert('Login successful!');
+
+      // Salva il token nel localStorage
+      localStorage.setItem('token', result.token);
+      window.location.href = 'LogSuccess.html';
     } else {
       alert('Login failed. Please check your credentials.');
     }
   } catch (error) {
     console.error('Error during login:', error);
+  }
+}
+
+// Sistema verifica di token
+window.onload = function () {
+  const token = localStorage.getItem('token');
+  console.log('Token from localStorage:', token);
+  if (token) {
+
+    // Verifica la validità del token con il server
+    fetch('http://localhost:3000/api/verify-token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': token
+      }
+    })
+      .then(response => {
+        // Controlla se la risposta è un JSON valido
+        if (response.headers.get('content-type').includes('application/json')) {
+          return response.json();
+        } else {
+          throw new Error('Server response is not JSON.');
+        }
+      })
+      .then(data => {
+        console.log('Risposta da API Verify-Token:', data);
+        if (data.success) {
+
+          // Il token è valido: cambia il pulsante di login in un pulsante di impostazioni
+          document.getElementById('loginButton').href = 'impostazioni.html';
+        } else {
+
+          // Il token non è valido: rimuovi il token e ritorna il pulsante di login allo stato iniziale
+          localStorage.removeItem('token');
+          document.getElementById('loginButton').href = 'login.html';
+        }
+      })
+      .catch(error => console.error('Error:', error));
+  } else {
+
+    // Non c'è token: ritorna il pulsante di login allo stato iniziale
+    document.getElementById('loginButton').href = 'login.html';
   }
 }
 
@@ -194,11 +108,11 @@ async function registrati() {
       body: JSON.stringify(utente),
     });
 
+    const data = await response.json();
     if (response.status === 201) {
       console.log('Utente registrato con successo');
       window.location.href = 'RegSuccess.html';
     } else {
-      const data = await response.json();
       console.error('Errore durante la registrazione dell\'utente:', data.message);
     }
   } catch (error) {
